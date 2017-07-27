07bb1a54754b3b0605235acd769cea0fb92611b2
Philip J. Reed <reed9999@uw.edu>
2017-07-27 14:49:46 -0700

    Intermediate.

--- replace.rb
+++ replace.rb
@@ -2,13 +2,17 @@
 # encoding: UTF-8
 class Replacer
 	class << self
+  $id = 0
   ORDER = ["eté", "aonu", "èêrish", "dlcm", "wfg", "ypbvkjxqz"]
     def single_line text
-      puts text
+      full_text = text.chomp
       rv = ORDER.collect do |to_replace|
-        replaced(text.chomp, to_replace).clone
+        text = replaced(text.chomp, to_replace)
+        text.clone
       end
+      rv = [$id, full_text] << rv
       puts rv.join "\t"
+      $id += 1
       rv
 		end
     def replaced text, to_replace
@@ -18,31 +22,10 @@ class Replacer
     end
 	end
 end
-input = File.open("input.txt") do |f|
+input = File.open("input.txt", :encoding => "utf-8" ) do |f|
   f.readlines
 end
 input.each do |v|
   Replacer.single_line v
 end
 
-verses = [
-"6 lui qui est de condition divine,
-il n'a pas regardé son égalité avec Dieu
-comme un butin à préserver,",
-"7 mais il s'est dépouillé lui-même
-en prenant une condition de serviteur,
-en devenant semblable aux êtres humains.
-Reconnu comme un simple homme,",
-"8 il s'est humilié lui-même
-en faisant preuve d’obéissance jusqu'à la mort,
-même la mort sur la croix.",
-"9 C'est aussi pourquoi Dieu l'a élevé à la plus haute place
-et lui a donné le nom
-qui est au-dessus de tout nom",
-"10 afin qu'au nom de Jésus
-chacun plie le genou
-dans le ciel, sur la terre et sous la terre",
-"11 et que toute langue reconnaisse
-que Jésus-Christ est le Seigneur,
-à la gloire de Dieu le Père.",
-]
